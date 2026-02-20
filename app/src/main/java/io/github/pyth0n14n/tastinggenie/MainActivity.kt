e221d0e345d068d4c33e618867664cf919a5f71e
Shoei Nashimoto <jaguar1014@hotmail.com>
2026-02-21 05:41:51 +0900

    Androidプロジェクトのひな型

---
+++ app/src/main/java/io/github/pyth0n14n/tastinggenie/MainActivity.kt
@@ -0,0 +1,47 @@
+package io.github.pyth0n14n.tastinggenie
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.pyth0n14n.tastinggenie.ui.theme.TastingGenie2ndAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TastingGenie2ndAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TastingGenie2ndAndroidTheme {
+        Greeting("Android")
+    }
+}
