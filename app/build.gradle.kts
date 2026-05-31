6de346a3a7476cd0c185b68b44c3e6b0a5c40e39
Shoei Nashimoto <jaguar1014@hotmail.com>
2026-05-31 13:26:56 +0900

    update versionCode and versionNmae for closed test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "io.github.pyth0n14n.tastinggenie"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
